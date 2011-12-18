19d73348169bee3b290fd5cbf7459519aac10bd9
Bill Roy <bill@bitlash.net>
2011-12-18 09:52:44 -0700

    sendnote is saving to redis

--- app.js
+++ app.js
@@ -35,10 +35,79 @@ app.post('/notesoup.php', function(req, res) {
 	console.log("Request body: " + typeof(req.body));
 	console.dir(req.body);
 
-//	console.log("Request: " + typeof(req));
-//	console.dir(req);
-
+	if (req.body.method == 'savenote') apisavenote(req, res);
+	else if (req.body.method == 'sync') apisync(req, res);
+	else if (req.body.method == 'sendnote') apisendnote(req, res);
+	else {
+		console.log("Error in request body: " + typeof(req.body));
+		console.dir(req.body);
+		console.log("*****");
+		console.dir(req);
+		res.send("Huh?");
+	}
+	
 });
+
+
+var updatelist;
+var clock = new Date();
+
+var redis = require("redis");
+var client = redis.createClient();		// port, host, options
+	
+client.on("error", function (err) {
+	console.log("Error " + err);
+});
+
+var nextnote = 1;
+
+
+function apisavenote(req, res) {
+
+	var folder = req.body.params['tofolder'];
+	var note = req.body.params['note'];
+
+	updatelist = [['beginupdate','']];
+	updatelist.push(apisavenoteworker(folder, note));
+	updatelist.push(['endupdate','']);
+
+	res.send(updatelist);
+}
+
+
+function apisavenoteworker(folder, note) {
+
+	if (!('id' in note)) {
+		//note['id'] = redis.incr('nextnote');
+		note['id'] = nextnote.toString();
+		nextnote++;
+	}
+
+	var jnote = JSON.stringify(note);
+
+	client.multi() 
+		.hset('notes/' + folder, note['id'], jnote)
+		.zadd('mtime/' + folder, clock.getTime(), note['id'])
+	 	.exec(function (err, replies) {
+			console.log("MULTI got " + replies.length + " replies");
+			replies.forEach(function (reply, index) {
+				console.log("Reply " + index + ": " + reply.toString());
+	        });
+        });
+
+	return ['updatenote', jnote];
+}
+
+
+
+function apisync(req, res) {
+	res.send(['say','sync...']);
+}
+
+function apisendnote(req, res) {
+	res.send(['say','send...']);
+}
+
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
