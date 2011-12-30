ae9725a902fac13be22478049154422d6e2b2595
Bill Roy <bill@bitlash.net>
2011-12-30 04:57:27 -0700

    license fiddling and acl fetch pipeline

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 Note Soup 
 ===
 
+### What is it?
+
+Note Soup is a sticky-note organizer on steroids for the web.  
+
+### Requirements
+
 See also INSTALL-EC2 for a bare Ubuntu machine buildout.
 
 ### Requirements
+
+- Git from http://git-scm.com/
 
 - Node.js 0.6.6 ish
 	http://blog.nodejs.org/2011/12/15/node-v0-6-6/
@@ -46,3 +54,24 @@ Run the server
 	$ node soup.js
 
 Browse to http://localhost:3000
+
+Create a user account and play.
+Hint: Create a note by typing in the command bar at the top
+
+### Administer
+
+The default administrative username is "system" and its default password is "frobozz88"
+
+The system user provides a home for the system templates (in the folder system/templates); 
+these are the templates that auto-populate the System part of the mytemplates folder.  
+
+The welcome and login page is configured in the system/welcome folder.  Edit this as appropriate
+for your site/project.
+
+You must be logged in as system to delete a user, and to create a user if open account creation
+is disabled.
+
+If you want to run a closed soup, it is strongly suggested to delete the "guest" user, or at
+least change its password from the default "kittylitter123".  Delete the guest login button
+in system/welcome, too.
+
--- notesoup.js
+++ notesoup.js
@@ -2,8 +2,34 @@
 	Note Soup server for node.js / redis
 	
 	Copyright 2011 by Bill Roy.
-	Licensed for use.  See LICENSE.
 
+	Redistribution and use in source and binary forms, with or without modification, 
+	are permitted provided that the following conditions are met:
+	
+		-	Redistributions of source code must retain the above copyright notice, 
+			this list of conditions and the following disclaimer.
+		-	Redistributions in binary form must reproduce the above copyright notice, 
+			this list of conditions and the following disclaimer in the documentation 
+			and/or other materials provided with the distribution.
+		-	Neither the name of the authors nor the names of its contributors may be
+			used to endorse or promote products derived from this software without 
+			specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+	A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*****/
+
+/*****
 Database use:
 
 	hash: notes/<user>/<folder>
@@ -69,7 +95,8 @@ dispatch: function(req, res) {
 	self.dir(req.body.params);
 
 	async.series(
-		[self.validatemethod, self.validateaccess, self.execute],
+		[self.validatemethod, self.loadfromacl, self.loadtoacl, 
+			self.validateaccess, self.execute],
 		function(err, reply) {
 			if (err) {
 				//self.senderror(err);
@@ -94,6 +121,36 @@ validatemethod: function(next) {
 	next(null);
 },
 
+loadfromacl: function(next) {
+	var self = NoteSoup;
+	self.loadfolderacl('fromfolder', next);
+},
+
+loadtoacl: function(next) {
+	var self = NoteSoup;
+	self.loadfolderacl('tofolder', next);
+},
+
+
+loadfolderacl: function(fieldname, next) {
+	var self = NoteSoup;
+	if (self.req.body.params[fieldname]) {
+		if (!self.req.acl) self.req.acl = {};
+		self.redis.hgetall(self.key_foldermeta(self.req.body.params[fieldname]), function (err, folderdata) {
+			if (err) {
+				self.log('Error fetching folder acl: ' + fieldname);
+				next('Error fetching folder acl.');
+				return;
+			}
+			self.req.acl[fieldname] = folderdata;
+			self.log('loadfolderacl: ');
+			self.dir(self.req.acl);
+			next(null);
+		});
+	}
+	else next(null);
+},
+
 validateaccess: function(next) {
 	var self = NoteSoup;
 	//next(null, 'Everybody has root!');		// enable this for wide-open soup
@@ -112,10 +169,10 @@ validateaccess: function(next) {
 
 		// determine the level of access required for the function
 		var accessmode;
-		if (aclcheck[1] == 'o') accessmode = 'owners';
-		else if (aclcheck[1] == 'e') accessmode = 'editors';
-		else if (aclcheck[1] == 's') accessmode = 'senders';
-		else accessmode = 'readers';
+		if (aclcheck[1] == 'o') accessmode = self.owners;
+		else if (aclcheck[1] == 'e') accessmode = self.editors;
+		else if (aclcheck[1] == 's') accessmode = self.senders;
+		else accessmode = self.readers;
 
 		// now determine whether we're checking tofolder or fromfolder
 		var folder;
