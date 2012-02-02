afe1b72635af724d93e31b30d51a84a3bbacb54b
Bill Roy <bill@bitlash.net>
2012-02-02 08:09:16 -0700

    makesoup.sh

---
+++ public/makesoup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Note Soup: Heroku bootstrap script
+#
+# Usage: makesoup.sh [name-for-soup]
+#
+# Example:	makesoup.sh chowder
+# => app is served at http://chowder.herokuapp.com
+#
+set -x
+date
+if [ $# -eq 0 ]
+then
+	export appdir="notesoup"
+else
+	export appdir="$1"
+fi
+echo "Application name:" $appdir
+git clone git://github.com/billroy/notesoup.git $1
+cd $appdir
+./scripts/heroku-start.sh $1
+date
